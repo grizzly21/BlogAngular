7608add9dae1cf8a49c6ba24b678eace954b24db
Grizzly <makintoch2104@gmail.com>
2022-05-27 21:57:07 +0300

    recovery backup

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import {AuthService} from "./service/auth.service";
+import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from "../app-routing.module";
+import {ReactiveFormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    RegisterComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers:[
+    AuthService
+  ]
+})
+export class AuthModule { }
---
+++ src/app/common/interfaces/IUser.ts
@@ -0,0 +1,9 @@
+/* For features */
+export class IUser{
+  constructor(
+    public _id: string,
+    public displayName: string,
+    public email: string,
+    public avatar: string
+  ) {}
+}
