4efa61a54b61ad0a6f7442edadfd980b1eb5883e
Grizzly <makintoch2104@gmail.com>
2022-06-17 09:05:43 +0300

    creqte form for user-profile component

--- src/app/stories/stories-routing.module.ts
+++ src/app/stories/stories-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { AddStoryComponent } from "./add-story/add-story.component";
 import { StoryComponent } from "./story/story.component";
 import { TopStoriesComponent } from "./top-stories/top-stories.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
 
 export const storiesRoutes: Routes = [
   {path: '', component: TopStoriesComponent},
   {path: 'story/:id', component: StoryComponent},
   {path: 'add-stories', component: AddStoryComponent},
+  {path: 'user-profile', component: UserProfileComponent},
 ]
 
 @NgModule({
