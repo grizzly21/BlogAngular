20bff73a58818becfa14b401633ad5f9216cfa30
DESKTOP-C643GPH\1 <makintoch2104@gmail.com>
2022-06-22 11:41:50 +0300

    create template for user profile

--- src/app/stories/user-profile/user-profile.component.ts
+++ src/app/stories/user-profile/user-profile.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent{
+
+  avatarFile!: File;
 
   constructor() { }
 
-  ngOnInit() {
+  onChangeAvatar(event: any){
+    this.avatarFile = <File>event.target.files[0];
+    console.log(this.avatarFile)
   }
 
 }
