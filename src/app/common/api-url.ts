bf1d3b0af02cd920b43f0f4728fa4025a7ddec4d
DESKTOP-C643GPH\1 <makintoch2104@gmail.com>
2022-06-22 11:37:07 +0300

    change api url, add router in main logo in header, finish template for user profile

--- src/app/common/api-url.ts
+++ src/app/common/api-url.ts
@@ -1,1 +1,2 @@
-export const apiUrl = 'https://homa-blog.azurewebsites.net/';
+//export const apiUrl = 'https://homa-blog.azurewebsites.net/';
+export const apiUrl = 'http://vbudnyi-001-site1.itempurl.com/';
